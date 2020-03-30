82b5d5d331e98b7f571eb7d4a153a6c268c6054c
Chris <cciric44@gmail.com>
2020-03-29 21:54:34 -0400

    more fragment work

--- app/src/main/java/com/hfad/workout/MainActivity.java
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,12 +1,13 @@
 package com.hfad.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +15,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onShowDetails(View view)
-    {
-        Intent intent = new Intent(this, DetailActivity.class);
-        startActivity(intent);
+//    public void onShowDetails(View view)
+//    {
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        startActivity(intent);
+//    }
+
+
+
+    @Override
+    public void itemClicked(long id) {
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null){
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+        else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -1,6 +1,7 @@
 package com.hfad.workout;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -17,6 +19,11 @@ import android.widget.TextView;
  */
 public class WorkoutListFragment extends ListFragment {
 
+    static interface Listener{
+        void itemClicked(long id);
+    }
+
+    private Listener listener;
 
     public WorkoutListFragment() {
         // Required empty public constructor
@@ -34,6 +41,21 @@ public class WorkoutListFragment extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
         setListAdapter(adapter);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+      super.onAttach(context);
+      this.listener = (Listener)context;
+    }
+
+
+    public void onListItemClick(ListView listView, View itemView, int position, long id){
+        if (listener != null){
+            listener.itemClicked(id);
+        }
+
     }
 
 }
